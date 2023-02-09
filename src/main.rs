c19f124005168821a3f10e1a094d1d7f81c68c06
Val Adler <spantamd@gmail.com>
2023-02-09 16:57:38 +0100

    Extract logic from main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -9,13 +10,22 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
+
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    };
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     print!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
@@ -24,9 +34,9 @@ struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &str> {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("not enough arguments")
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
