93ca0122dc6763236d5df0852dd3b8a2cd5c03db
Val Adler <spantamd@gmail.com>
2023-02-10 18:06:04 +0100

    Add search functionality and tests

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,69 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    print!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "Id";
+        let contents = "\
+Lorem ipsum dolor sit amet.
+Id adipisci harum aut vero dolorem
+vel consequatur veniam aut quis 
+cupiditate et maxime repellat.
+LOREM IPSuM DOLOR SIt AMET.";
+        let result = vec!["Id adipisci harum aut vero dolorem"];
+        assert_eq!(result, search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "doLor";
+        let contents = "\
+Lorem ipsum dolor sit amet.
+Id adipisci harum aut vero dolorem
+vel consequatur veniam aut quis 
+cupiditate et maxime repellat.
+LOREM IPSuM DOLOR SIt AMET.";
+        let result = vec![
+            "Lorem ipsum dolor sit amet.",
+            "Id adipisci harum aut vero dolorem",
+            "LOREM IPSuM DOLOR SIt AMET.",
+        ];
+        assert_eq!(result, search_case_insensitive(query, contents));
+    }
 }
