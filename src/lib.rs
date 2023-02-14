0791cbca99f79e819940609b291bc5ba2d453a4e
Val Adler <spantamd@gmail.com>
2023-02-14 22:58:44 +0100

    Refactor with iterators

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,20 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let (args, flags) = Self::find_flags(args);
-        let ignore_case_flag = Self::parse_flags(flags)?;
+        let query = match args.next() {
+            Some(query) => query,
+            None => return Err("Din't get a query string"),
+        };
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let file_path = match args.next() {
+            Some(file_path) => file_path,
+            None => return Err("Didn't get a filepath"),
+        };
+
+        let ignore_case_flag = Self::parse_flags(args)?;
 
         let ignore_case = env::var("IGNORE_CASE").map_or_else(
             |_| ignore_case_flag,
@@ -36,22 +40,7 @@ impl Config {
         })
     }
 
-    fn find_flags(args: &[String]) -> (Vec<&String>, Vec<&String>) {
-        let mut flags = Vec::new();
-        let mut remaining_args = Vec::new();
-
-        for a in args {
-            if a.starts_with("--") {
-                flags.push(a);
-            } else {
-                remaining_args.push(a)
-            }
-        }
-
-        (remaining_args, flags)
-    }
-
-    fn parse_flags(flags: Vec<&String>) -> Result<bool, &'static str> {
+    fn parse_flags(flags: impl Iterator<Item = String>) -> Result<bool, &'static str> {
         let mut ignore_case_flag = false;
 
         for flag in flags {
@@ -83,28 +72,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-
-    result
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut result = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
-
-    result
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::process;
 use minigrep::Config; 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = env::args();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
